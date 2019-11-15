872187f601be4b48f5e2056aa5d58076f03a14c0
Julie Paturel <patujulie@gmail.com>
2019-11-15 11:36:10 +0100

    changes seed and navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'json'
+require 'open-uri'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,10 +7,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Cocktail.destroy_all
+Dose.destroy_all
 Ingredient.destroy_all
 
-require 'json'
-require 'open-uri'
+Cocktail.create!(name: "Mojito")
+Cocktail.create!(name: "Cuba Libre")
+Cocktail.create!(name: "Spritz")
 
 
 url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
@@ -16,16 +21,9 @@ ingredients_list = open(url).read
 list = JSON.parse(ingredients_list)
 
 list['drinks'].each do |ingredient|
-Ingredient.create(name: ingredient["strIngredient1"])
+  Ingredient.create(name: ingredient["strIngredient1"])
 end
 
-Cocktail.destroy_all
-Cocktail.create!(name: "Mojito")
-Cocktail.create!(name: "Cuba Libre")
-Cocktail.create!(name: "Spritz")
-
-Dose.destroy_all
 Dose.create!(description: "5cl", ingredient: Ingredient.first, cocktail: Cocktail.first)
 Dose.create!(description: "10 cl", ingredient: Ingredient.second, cocktail: Cocktail.first)
 Dose.create!(description: "12 g", ingredient: Ingredient.last, cocktail: Cocktail.first)
-
